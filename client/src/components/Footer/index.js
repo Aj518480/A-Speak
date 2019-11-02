379e442e48f367703b5682c3e68999b27b66eef5
aj518480 <allisoncordovez@gmail.com>
2019-11-02 00:01:09 -0400

    made some changes in the footer and included some text in the Jumbotron about the project before the main buttons. I also fixed the background image and container

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,10 +10,7 @@ function Footer() {
   
   <div className="container-fluid text-center text-md-left">
   
-    <div className="row">
-
-      
-      <section className="team-section text-center my-5"></section>
+    <div className="row p-0">
 
         
         <h2 className="h1-responsive font-weight-bold my-5 col-12 creatorsTitle">Creators of A-Speak</h2>
@@ -25,7 +22,7 @@ function Footer() {
           
           <div className="col-lg-3 col-md-6 col-sm-12 mb-lg-0 mb-5 avatar">
 
-              <img src="https://avatars3.githubusercontent.com/u/53452871?s=460&v=4"height="350px"width="300px"
+              <img src="https://avatars3.githubusercontent.com/u/53452871?s=460&v=4"height="50%"width="30%"
                   className="rounded-circle z-depth-1 col-12" alt="AllisonJuarez"></img>
             
 
@@ -34,7 +31,7 @@ function Footer() {
             <p className="white-text text-center"> I am a Full-Stack Web Developer. A mom of two
               great kids. My son has autism and  he is my inspiration for this project. I know the struggle from trying to find free and good resources for kids in the spectrum. So I dedicate this to him and everyone who can benefit from this application.
               </p>
-            <ul className="list-unstyled mb-0 icons">
+            <ul className="list-unstyled mb-5 icons">
               
               <a className="p-2 fa-lg fb-ic" href="https://www.linkedin.com/in/allison-juarez-6712a518b/" target="_blank">
                 <i className="fab fa-linkedin white-text"> </i>
@@ -52,23 +49,24 @@ function Footer() {
           </div>
 
           <div className ="col"></div>
+          
 
           
-          <div className="col-lg-3 col-md-6 col-sm-12 mb-lg-0 mb-5 avatar">
+          <div className="col-lg-3 col-md-6 col-sm-12  avatar">
             
-             <img src="https://avatars2.githubusercontent.com/u/37284788?s=460&v=4" height="350px"width="300px"
+             <img src="https://avatars2.githubusercontent.com/u/37284788?s=460&v=4" height="55%"width="30%"
                 className="rounded-circle z-depth-1 col-12" alt="Gelissa Leveielle"></img>
             
             <h5 className="font-weight-bold mb-3 titleBio">Gelissa Leveille</h5>
             <p className="text-uppercase text-center"><strong>Full- Stack Developer</strong></p>
-            <p className="white-text text-center">I’m a full-stack developer, currently based in Orlando.</p>
-            <ul className="list-unstyled mb-0 icons">
+            <p className="white-text text-center">Gelissa leveille is a full-stack javascript developer from sunny florida. her likes include books, movies, and css.  due to commitment issues, all changes will be made in the dev tools until further notice.</p>
+            <ul className="list-unstyled mb-5 icons">
               
-              <a className="p-2 fa-lg fb-ic" href="" target="_blank">
+              <a className="p-2 fa-lg fb-ic" href="http://linkedin.com/in/gelissaleveille" target="_blank">
                 <i className="fab fa-linkedin white-text"> </i>
               </a>
              
-              <a className="p-2 fa-lg ins-ic" href="" target="_blank">
+              <a className="p-2 fa-lg ins-ic" href="http://github.com/gelissa" target="_blank">
                 <i className="fab fa-github white-text"> </i>
               </a>
               
@@ -79,10 +77,10 @@ function Footer() {
           </div>
           </div>
           </div>
-  
+         
   <div className="footer-copyright text-center py-3 smallFooter">© 2019 Copyright
-    
   </div>
+  
   
 </footer>
 
