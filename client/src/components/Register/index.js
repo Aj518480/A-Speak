0a43bfb640f8b73a0d10fa4f2195632374961557
gelissa <gelissaleveille@yahoo.com>
2019-11-04 14:23:51 -0500

    added container component to login and register page

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RegisterForm from "../RegisterForm";
+import { Container } from "../Grid";
 //The component for doing the actual signup of the User
 class Register extends React.Component {
 	state = {
@@ -28,8 +29,10 @@ class Register extends React.Component {
 	render() {
 		return (
 			<div>
+				<Container>
 				<h4>Register a New User</h4>
 				<RegisterForm onRegister={this.register} />
+				</Container>
 			</div>
 		)
 	}
