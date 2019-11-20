606f64113f0bdfbfa3059fc836a2fd51d6a60723
aj518480 <allisoncordovez@gmail.com>
2019-11-20 13:14:37 -0500

    heroku deployment changes

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static('public'))
 
 /* Serve up static assets (usually on heroku) */
 if (process.env.NODE_ENV === "production") {
-  app.use(passport.session()); app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(passport.session()); app.use(express.static(path.join(__dirname, 'client/build')));
 };
 
 /* FileUpload package grabbing the image and setting a path for it */
